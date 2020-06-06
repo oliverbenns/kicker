412b7d6891d211905a5642a90364b0d7dc608124
Oliver Benns <oliverbenns@gmail.com>
2020-06-06 17:20:42 +0100

    Update to use csv from url

--- check-domain-availability/main.go
+++ check-domain-availability/main.go
@@ -1,31 +1,79 @@
 package main
 
 import (
+	"encoding/csv"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/domainr/whois"
 	"github.com/oliverbenns/kicker/notifications"
-	"log"
-	"os"
-	"strings"
 )
 
-func CreateHandler(notify notifications.Notifier) func() {
-	return func() {
-		domains := strings.Split(os.Getenv("WANTED_DOMAINS"), ",")
+type Ctx struct{}
 
-		for _, domain := range domains {
-			if IsDomainAvailable(domain) {
-				message := domain + " is available."
-				log.Print(message)
-				notify(message)
+func CreateHandler(notify notifications.Notifier, url string) func() {
+	return func() {
+		resp, err := http.Get(url)
+
+		if err != nil {
+			panic("Error obtaining url list")
+		}
+
+		defer resp.Body.Close()
+
+		r := csv.NewReader(resp.Body)
+
+		headers, err := r.Read()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for {
+			row, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			var name string
+			for i, column := range row {
+				if i == 0 {
+					name = row[i]
+					continue
+				}
+				val, err := strconv.Atoi(column)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if val > 0 {
+					tld := headers[i]
+					domain := name + "." + tld
+
+					if IsDomainAvailable(domain) {
+						message := domain + " is available."
+						log.Print(message)
+						notify(message)
+					}
+				}
 			}
 		}
 	}
 }
 
-// @TODO: Is there a better way to do this? func Handler() = CreateHandler(NotifyAvailable)
 func Handler() {
-	handler := CreateHandler(notifications.Notify)
+	url := os.Getenv("DOMAINS_CSV_URL")
+	handler := CreateHandler(notifications.Notify, url)
 	handler()
 }
 
@@ -39,7 +87,10 @@ func IsDomainAvailable(domain string) bool {
 		return false
 	}
 
-	return strings.Contains(string(out.Body), "No match for")
+	// No match for - most domains
+	// No Data Found - .co
+	str := string(out.Body)
+	return strings.Contains(str, "No match for") || strings.Contains(str, "No Data Found")
 }
 
 func main() {
